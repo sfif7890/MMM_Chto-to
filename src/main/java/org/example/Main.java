ae925c192f2cce1087668036c09067593eb00a32
sfif7890 <dvornikov.sashakpc1882020@gmail.com>
2023-10-20 12:13:07 +0300

    DA

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\033[3;31m⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⣿⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⠄⣿⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⣿⣿⣿⣿⣿");
+        System.out.println("⣿⠄⣿⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⣿⣿⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⣿⠄⣿⣿⣿⣿⣿⠄⠄⠄⣿⣿⣿⠄⠄⣿⣿⣿");
+        System.out.println("⣿⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⠄⠄⠄⠄⠄⣿⣿⣿⠄⠄⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⣿⠄⣿⣿⣿⠄⠄⠄⠄⠄⣿⣿⣿⣿⠄⠄⣿⣿");
+        System.out.println("⣿⠄⣿⠄⠄⠄⠄⣿⣿⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⠄⠄⣿⣿");
+        System.out.println("⣿⠄⣿⠄⠄⠄⠄⣿⣿⣿⠄⠄⠄⠄⠄⣿⣿⣿⣿⠄⠄⠄⣿");
+        System.out.println("⣿⠄⣿⣿⣿⣿⠄⣿⣿⣿⣿⠄⣿⠄⠄⠄⣿⣿⣿⠄⠄⠄⣿");
+        System.out.println("⣿⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⣿⠄⠄⠄⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⣿⠄⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠄⠄⣿⣿");
+        System.out.println("⣿⠄⣿⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠄⣿⣿⣿");
+        System.out.println("⣿⠄⣿⠄⠄⠄⠄⣿⣿⣿⣿⠄⣿⣿⣿⠄⠄⠄⠄⠄⣿⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⣿⠄⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿");
+        System.out.println("⣿⠄⠄⠄⠄⠄⠄⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿");
+        System.out.println("⣿⠄⣿⣿⣿⣿⠄⣿⠄⠄⠄⣿⠄⠄⠄⠄⣿⣿⣿⣿⠄⠄⣿");
+        System.out.println("⣿⠄⣿⠄⠄⣿⠄⣿⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⣿⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⠄⣿⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        }
+    }
